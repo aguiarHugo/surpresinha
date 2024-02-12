a1ae8aa34fa7793c776b60dfd68fcd50da1bca68
aguiarHugo <aguiarhugofx@gmail.com>
2024-02-12 16:33:50 -0300

    arquivo criado para mapear as cores usadas no dialog e nos botões

---
+++ lib/widgets/button_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Map<String, Color> lotteryColors = {
+  'megasena': Color(0xFF00ab69),
+  'lotofacil': Color(0xFF7f3992),
+  'quina': Color(0xFF015ea1),
+  'lotomania': Color(0xFFf79c31),
+  'federal' : Color(0xFF015ea1),
+};
