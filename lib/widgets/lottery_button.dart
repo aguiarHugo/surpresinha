cc65fec29377db7a743fb3fba6e2007995f87ca8
aguiarHugo <aguiarhugofx@gmail.com>
2024-02-12 16:34:10 -0300

    estilização do botão

--- lib/widgets/lottery_button.dart
+++ lib/widgets/lottery_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../api/lottery_api.dart';
+import 'button_colors.dart';
+
 
 class LotteryButton extends StatelessWidget {
   final String lotteryName;
@@ -8,9 +10,20 @@ class LotteryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => fetchLotteryResult(context, lotteryName),
-      child: Text(lotteryName.toUpperCase()),
+    // Busca a cor da loteria no mapa, com uma cor padrão se não encontrada
+    final color = lotteryColors[lotteryName.toLowerCase()] ?? Colors.grey;
+
+    return SizedBox(
+      width: 200,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () => fetchLotteryResult(context, lotteryName),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color, // Aplica a cor do botão baseada no nome da loteria
+          foregroundColor: Colors.white, // Cor do texto dentro do botão, para garantir contraste
+        ),
+        child: Text(lotteryName.toUpperCase()),
+      ),
     );
   }
 }
